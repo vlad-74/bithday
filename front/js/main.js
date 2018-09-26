a937f0510ef671cce12d714be65bf96b5e4f1dcc
Vlad <kurguzov74@gmail.com>
2018-09-26 18:10:50 +0300

    validation

--- front/js/main.js
+++ front/js/main.js
@@ -8,14 +8,25 @@ function randomIntFromInterval(min, max) {
 
 function sendArray(arraySend){
     console.log(arraySend);
+    document.getElementById("sub").style.pointerEvents = "none";
+    document.getElementById("sub").style.opacity = ".3";
+    for (let i = 0; i < 2; i++) {
+        document.forms[0][i].value ="";
+    }
+}
+
+function cleanError() {
+    var array = [0, 1];
+    if (document.getElementById("error")) {
+        document.getElementById("error").style.display = "none";
+        for (let i = 0; i < array.length; i++) {
+            var j = array[i];
+            document.forms[0][j].classList.remove("is-invalid");
+        }
+    }
 }
 
 function validForm() {
-    document.getElementById("sub").style.pointerEvents = "none";
-    document.getElementById("sub").style.opacity = ".3";
-
-    // var email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
-    var num_regex = /^[0-9]/i;
     var typeError;
 
     var array = [0, 1];
@@ -26,53 +37,51 @@ function validForm() {
         let j = array[i];
         // console.log(document.forms[0], );
         arraySend.push(document.forms[0][j].value.trim());
+    
+        if (
+            !document.forms[0][j].validity.valid ||
+            !document.forms[0][j].value.trim().length 
+        ) {
+            er = true;
+            document.forms[0][j].focus();
+
+            document.forms[0][j].classList.add("is-invalid");
+
+            typeError = document.getElementById("error");
+
+            console.log("111", document.forms);
+
+            if (j === 0) {
+                typeError.innerText = "Заполните поле Имя и Фамилия";
+            }
+            if (j === 1) {
+                typeError.innerText = "Добавьте свою историю";
+            }
+
+            typeError.style.display = "block";
+        }
     }
-
-    sendArray(arraySend);
-
-    //     if (
-    //         !document.forms[0][j].validity.valid ||
-    //         !document.forms[0][j].value.trim().length ||
-    //         (j === 3 && !email_regex.test(document.forms[0][j].value))
-    //     ) {
-    //         er = true;
-    //         document.forms[0][j].focus();
-
-    //         document.forms[0][j].classList.add("is-invalid");
-
-    //         typeError = document.getElementById("error");
-
-    //         console.log("111", document.forms);
-
-    //         if (j === 3 && !email_regex.test(document.forms[0][j].value) && document.forms[0][j].value.trim().length) {
-    //             typeError.innerText = "Не корректный адрес";
-    //         } else if (j === 3) {
-    //             typeError.innerText = "Укажите почту";
-    //         }
-    //         if (j === 2) {
-    //             typeError.innerText = "Заполните поле Имя и Фамилия";
-    //         }
-    //         if (j === 0) {
-    //             typeError.innerText = "Добавьте свою историю";
-    //         }
-
-    //         typeError.style.display = "block";
-    //     }
-    // }
-    // if (!er) {
-    //     document.getElementById("error").style.display = "none";
-    //     send();
-    // }
+    if (!er) {
+        document.getElementById("error").style.display = "none";
+        sendArray(arraySend);
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function() {//Аналог $(document).ready(function(){
+
+    document.getElementById("name1").addEventListener('keydown', function (ev) {
+        cleanError();
+    });
+
+    document.getElementById("txt1").addEventListener('keydown', function (ev) {
+        cleanError();
+    });
 
     document.getElementById("sub").addEventListener('click', function (ev) {
         validForm();
     });
 
     function rot(ran){
-        // $("#img").rotate({ angle: 45 });
         $("#baraban").rotate({
           angle: 0,
           animateTo: ran,
