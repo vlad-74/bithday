fa39ed8f8a46bdc7ab7a9caa9bdc2971340f7e6b
Vlad <kurguzov74@gmail.com>
2018-09-28 10:13:55 +0300

    шарики

--- front-старт с контейнером/js/main.js
+++
@@ -1,7 +0,0 @@
-import * as data from "./data";
-import "../style/main.scss";
-import _ from 'lodash';
-
-document.addEventListener("DOMContentLoaded", function() {//Аналог $(document).ready(function(){
-
-});
