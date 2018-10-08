c0e6e8a7487312eab20d1b43e377a2338d92e13b
Vlad <kurguzov74@gmail.com>
2018-10-08 11:04:14 +0300

    скролл на меню3

--- dist/bundle.js
+++ dist/bundle.js
@@ -484,7 +484,7 @@
 	            var id = $(this).attr("href"),
 
 	            //узнаем высоту от начала страницы до блока на который ссылается якорь
-	            top = $(id).offset().top;
+	            top = $(id).offset().top - 135;
 
 	            //анимируем переход на расстояние - top за 1500 мс
 	            $("body,html").animate({ scrollTop: top }, 1500);
